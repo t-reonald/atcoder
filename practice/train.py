5c99975879f107a272b0ddbc138161f6a2c25edc
t_reonald <takahashi.reo828@gmail.com>
2023-02-01 01:01:16 +0900

    update

--- practice/train.py
+++ practice/train.py
@@ -1,1 +1,8 @@
-print("hello world")
+n, p, q, r,s = map(int, input().split())
+a = list(map(int, input().split()))
+
+for i in range(p,q+1):
+    a[i-1],a[r-p+i-1] = a[r-p+i-1],a[i-1]
+
+print(*a)
+    
